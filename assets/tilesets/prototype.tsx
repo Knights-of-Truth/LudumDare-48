e93d670fa32ef7ae2c3efd25a9cd8c0e5bf29994
Rami Sabbagh <ramilego4game@gmail.com>
2021-04-24 01:21:59 +0300

    Enable automatic tiles flipping and rotating

--- assets/tilesets/prototype.tsx
+++ assets/tilesets/prototype.tsx
@@ -3,6 +3,7 @@
  <editorsettings>
   <export target="prototype.json" format="json"/>
  </editorsettings>
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="1"/>
  <image source="prototype.png" width="256" height="256"/>
  <tile id="34" type="crate"/>
  <wangsets>
@@ -22,23 +23,13 @@
    <wangtile tileid="66" wangid="0,1,0,0,0,0,0,1"/>
    <wangtile tileid="67" wangid="0,0,0,0,0,0,0,1"/>
   </wangset>
-  <wangset name="Unnamed Set" type="mixed" tile="-1">
-   <wangcolor name="Wire" color="#ff0000" tile="-1" probability="1"/>
+  <wangset name="Pipe" type="edge" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
    <wangtile tileid="6" wangid="1,0,0,0,1,0,0,0"/>
-   <wangtile tileid="7" wangid="0,0,0,0,1,0,0,0"/>
-   <wangtile tileid="8" wangid="0,0,1,0,0,0,1,0"/>
-   <wangtile tileid="9" wangid="0,0,1,0,1,0,0,0"/>
-   <wangtile tileid="10" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="7" wangid="0,0,1,0,1,0,0,0"/>
    <wangtile tileid="38" wangid="0,0,1,0,0,0,0,0"/>
    <wangtile tileid="39" wangid="1,0,1,0,1,0,1,0"/>
-   <wangtile tileid="40" wangid="0,0,0,0,0,0,1,0"/>
-   <wangtile tileid="41" wangid="1,0,1,0,0,0,0,0"/>
-   <wangtile tileid="42" wangid="1,0,0,0,0,0,1,0"/>
-   <wangtile tileid="71" wangid="1,0,0,0,0,0,0,0"/>
-   <wangtile tileid="72" wangid="0,0,1,0,1,0,1,0"/>
-   <wangtile tileid="73" wangid="1,0,0,0,1,0,1,0"/>
-   <wangtile tileid="104" wangid="1,0,1,0,1,0,0,0"/>
-   <wangtile tileid="105" wangid="1,0,1,0,0,0,1,0"/>
+   <wangtile tileid="40" wangid="0,0,1,0,1,0,1,0"/>
   </wangset>
  </wangsets>
 </tileset>
