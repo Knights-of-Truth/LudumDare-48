12811b27f22f2edcce60988b02967c4846fff3a0
Rami Sabbagh <ramilego4game@gmail.com>
2021-04-24 00:17:23 +0300

    Extended tileset

--- assets/tilesets/prototype.tsx
+++ assets/tilesets/prototype.tsx
@@ -1,23 +1,44 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Prototype" tilewidth="8" tileheight="8" tilecount="1024" columns="32" objectalignment="center">
+<tileset version="1.5" tiledversion="1.6.0" name="Prototype" tilewidth="8" tileheight="8" tilecount="1024" columns="32" objectalignment="center">
  <editorsettings>
   <export target="prototype.json" format="json"/>
  </editorsettings>
  <image source="prototype.png" width="256" height="256"/>
- <terraintypes>
-  <terrain name="Matter" tile="100"/>
- </terraintypes>
- <tile id="1" terrain=",,,0"/>
- <tile id="2" terrain=",,0,0"/>
- <tile id="3" terrain=",,0,"/>
- <tile id="4" terrain="0,0,0,"/>
- <tile id="5" terrain="0,0,,0"/>
- <tile id="33" terrain=",0,,0"/>
- <tile id="34" type="crate" terrain="0,0,0,0"/>
- <tile id="35" terrain="0,,0,"/>
- <tile id="36" terrain="0,,0,0"/>
- <tile id="37" terrain=",0,0,0"/>
- <tile id="65" terrain=",0,,"/>
- <tile id="66" terrain="0,0,,"/>
- <tile id="67" terrain="0,,,"/>
+ <tile id="34" type="crate"/>
+ <wangsets>
+  <wangset name="Terrains" type="corner" tile="-1">
+   <wangcolor name="Matter" color="#ff0000" tile="100" probability="1"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="4" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="5" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="33" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="35" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="37" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="65" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="66" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="67" wangid="0,0,0,0,0,0,0,1"/>
+  </wangset>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="Wire" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="1,0,0,0,1,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,0,0,1,0,0,0"/>
+   <wangtile tileid="8" wangid="0,0,1,0,0,0,1,0"/>
+   <wangtile tileid="9" wangid="0,0,1,0,1,0,0,0"/>
+   <wangtile tileid="10" wangid="0,0,0,0,1,0,1,0"/>
+   <wangtile tileid="38" wangid="0,0,1,0,0,0,0,0"/>
+   <wangtile tileid="39" wangid="1,0,1,0,1,0,1,0"/>
+   <wangtile tileid="40" wangid="0,0,0,0,0,0,1,0"/>
+   <wangtile tileid="41" wangid="1,0,1,0,0,0,0,0"/>
+   <wangtile tileid="42" wangid="1,0,0,0,0,0,1,0"/>
+   <wangtile tileid="71" wangid="1,0,0,0,0,0,0,0"/>
+   <wangtile tileid="72" wangid="0,0,1,0,1,0,1,0"/>
+   <wangtile tileid="73" wangid="1,0,0,0,1,0,1,0"/>
+   <wangtile tileid="104" wangid="1,0,1,0,1,0,0,0"/>
+   <wangtile tileid="105" wangid="1,0,1,0,0,0,1,0"/>
+  </wangset>
+ </wangsets>
 </tileset>
