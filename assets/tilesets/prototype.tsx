a81ac72f8b2fe64e9973c919cfd57d81692c8af7
Rami Sabbagh <ramilego4game@gmail.com>
2021-04-24 17:27:00 +0300

    Play more with the terrain brush

--- assets/tilesets/prototype.tsx
+++ assets/tilesets/prototype.tsx
@@ -32,5 +32,33 @@
    <wangtile tileid="39" wangid="1,0,1,0,1,0,1,0"/>
    <wangtile tileid="40" wangid="0,0,1,0,1,0,1,0"/>
   </wangset>
+  <wangset name="Wall" type="mixed" tile="-1">
+   <wangcolor name="Solid" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="White" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="Pipe" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="6" wangid="3,0,0,0,3,0,0,0"/>
+   <wangtile tileid="7" wangid="0,0,3,0,3,0,0,0"/>
+   <wangtile tileid="38" wangid="0,0,3,0,0,0,0,0"/>
+   <wangtile tileid="39" wangid="3,0,3,0,3,0,3,0"/>
+   <wangtile tileid="40" wangid="0,0,3,0,3,0,3,0"/>
+   <wangtile tileid="98" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="99" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="100" wangid="0,0,2,0,0,0,0,0"/>
+   <wangtile tileid="101" wangid="0,0,0,0,2,0,2,0"/>
+   <wangtile tileid="102" wangid="2,0,0,0,2,0,0,0"/>
+   <wangtile tileid="103" wangid="3,0,0,0,2,0,0,0"/>
+   <wangtile tileid="104" wangid="0,0,3,0,3,0,2,0"/>
+   <wangtile tileid="105" wangid="2,0,3,0,3,0,3,0"/>
+   <wangtile tileid="106" wangid="3,0,2,0,3,0,2,0"/>
+   <wangtile tileid="107" wangid="3,0,0,0,3,0,2,0"/>
+   <wangtile tileid="130" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="131" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="133" wangid="2,0,2,0,2,0,2,0"/>
+   <wangtile tileid="134" wangid="2,0,0,0,2,0,2,0"/>
+   <wangtile tileid="136" wangid="0,0,2,0,2,0,3,0"/>
+   <wangtile tileid="137" wangid="3,0,2,0,2,0,2,0"/>
+   <wangtile tileid="138" wangid="2,0,3,0,2,0,3,0"/>
+   <wangtile tileid="139" wangid="2,0,0,0,2,0,3,0"/>
+  </wangset>
  </wangsets>
 </tileset>
